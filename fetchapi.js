5500440e29fc131b51a3b4c2eb33797597d8c8c1
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-06-22 18:23:36 +0300

    updated fetchandDisplay solution

--- fetchapi.js
+++ fetchapi.js
@@ -4,22 +4,28 @@ const urls = [
     'https://jsonplaceholder.typicode.com/todos/3'
 ];
 
-function fetch(urls) {
-    return fetch(urls)
-    .then(response => response.json())
-    .then((data) => {
-        try {
-            console.log(data);
-        } catch (error) {
+ function fetchAndDisplay(urls) {
+    urls.map ((url) => fetch(url))
+        .then((response) => response.json())
+        .then((data) => {
+            try {
+                console.log(data);
+            } catch (error) {
+                console.log(error);
+            }
+        })
+        .catch((error) => {
             console.log(error);
-        }
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-}
+        })
 
-// Iterate over the URLs and make AJAX calls
-urls.forEach(url => {
-    fetch(url);
-  });
+ }
+
+
+
+
+
+
+ 
+
+ 
+
